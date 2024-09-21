a79a06d7456f78f8761fa1a05adf6a433e383442
matskye <matskye_wow@hotmail.com>
2024-09-21 13:26:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const prompts = [
     { jp: "まるが言う: <ruby>手<rt>て</rt></ruby>を<ruby>叩<rt>たた</rt></ruby>いてください。", en: "Maru says: Clap your hands.", image: "images/maru_clap.png" },
     { jp: "まるが言う: こんにちはと<ruby>言<rt>い</rt></ruby>ってください。", en: "Maru says: Say hello.", image: "images/maru_say_hello.png" },
     { jp: "まるが言う: <ruby>手<rt>て</rt></ruby>を<ruby>見<rt>み</rt></ruby>せてください。", en: "Maru says: Show your hands.", image: "images/maru_show_hands.png" },
+    { jp: "まるが言う: <ruby>左手<rt>ひだりて</rt></ruby>を<ruby>見<rt>み</rt></ruby>せてください。", en: "Maru says: Show your left hand.", image: "images/maru_show_left_hand.png" },
+    { jp: "まるが言う: <ruby>右手<rt>みぎて</rt></ruby>を<ruby>見<rt>み</rt></ruby>せてください。", en: "Maru says: Show your right hand.", image: "images/maru_show_right_hand.png" },
+    { jp: "まるが言う: <ruby>足<rt>あし</rt></ruby>を<ruby>上<rt>あ</rt></ruby>げてください。", en: "Maru says: Raise your foot.", image: "images/maru_raise_foot.png" },
+    { jp: "まるが言う: キックしてください。", en: "Maru says: Please Kick.", image: "images/maru_kick.png" },
+    { jp: "まるが言う: <ruby>左足<rt>ひだりあし</rt></ruby>を<ruby>上<rt>あ</rt></ruby>げてください。", en: "Maru says: Raise your left leg.", image: "images/maru_raise_left_foot.png" },
+    { jp: "まるが言う: <ruby>右足<rt>みぎあし</rt></ruby>を<ruby>上<rt>あ</rt></ruby>げてください。", en: "Maru says: Raise your right leg.", image: "images/maru_raise_right_foot.png" },
     // Add more prompts and corresponding images here
 ];
 
